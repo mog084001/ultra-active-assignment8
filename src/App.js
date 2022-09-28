ac25aa8d362231b7792e7076a9b862c5f6fbdb8f
mog084001 <mong.86duet@gmail.com>
2022-09-29 01:55:43 +0600

    added create club-container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Club from './components/Club/Club';
 
 function App() {
   return (
     <div className="App">
-
+      <Club></Club>
     </div>
   );
 }
--- src/components/Club/Club.js
+++ src/components/Club/Club.js
@@ -4,8 +4,19 @@ import './Club.css';
 const Club = () => {
     return (
         <div>
-
+            <div className='gym-club'>
+                <h1>Gym-Active-Club</h1>
+            </div>
+            <div className='club-container'>
+                <div>
+                    <h1>This is club</h1>
+                </div>
+                <div>
+                    <h3>Information Summary</h3>
+                </div>
+            </div>
         </div>
+
     );
 };
 

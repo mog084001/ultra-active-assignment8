d41b4212f52a78d3fecede648772915c967a8430
mog084001 <mong.86duet@gmail.com>
2022-09-29 16:34:45 +0600

    added product and product-container

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './Product.css';
 
 const Product = (props) => {
-    const { img, nameTitle, timeRequired } = props.club;
+    const { img, nameTitle, timeRequried } = props.club;
     return (
         <div className='product'>
             <img src={img} alt="" />
+            <div className='product-info'>
+                <p>{nameTitle}</p>
+                <p>Time requried: {timeRequried}</p>
+            </div>
 
         </div>
     );
